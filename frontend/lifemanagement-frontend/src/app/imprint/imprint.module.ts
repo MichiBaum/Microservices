e2a99cc456e89a552f538c5b6f2b30f2e7827659
MichiBaum <michael_baumberger@gmx.ch>
2020-06-16 10:49:54 +0200

    Added impress text with translation

--- frontend/lifemanagement-frontend/src/app/imprint/imprint.module.ts
+++ frontend/lifemanagement-frontend/src/app/imprint/imprint.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ImprintComponent } from './imprint.component';
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
   declarations: [ImprintComponent],
-  imports: [
-    CommonModule
-  ],
+	imports: [
+		CommonModule,
+		TranslateModule
+	],
   exports: [ImprintComponent]
 })
 export class ImprintModule { }
