e6c04f2e2cb52d8d9f51b6797a78da76717713d6
MichiBaum <michael_baumberger@gmx.ch>
2020-06-16 13:18:58 +0200

    Change token validate from 10 to 2 days

--- backend/lifemanagement-backend/src/main/kotlin/com/michibaum/lifemanagementbackend/security/SecurityConstants.kt
+++ backend/lifemanagement-backend/src/main/kotlin/com/michibaum/lifemanagementbackend/security/SecurityConstants.kt
@@ -2,7 +2,7 @@ package com.michibaum.lifemanagementbackend.security
 
 internal object SecurityConstants {
     const val SECRET = "SecretKeyToGenJWTs"
-    const val EXPIRATION_TIME: Long = 1000 * 3600 * 24 * 10 // 10 days
+    const val EXPIRATION_TIME: Long = 1000 * 3600 * 24 * 2 // 2 days
     const val TOKEN_PREFIX = "Bearer "
     const val HEADER_STRING = "Authorization"
 }
