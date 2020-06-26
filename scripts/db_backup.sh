7cb886389cbb12e90a7f4db5e7565664d5ff9825
MichiBaum <michael_baumberger@gmx.ch>
2020-06-26 09:41:28 +0200

    Update db_backup.sh

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -7,34 +7,34 @@ DATETIME=$(date +%Y%m%d%H%M%S)
 if [ -d "$DIRBACKUP" ]; then
 
   ### Take action if $DIRBACKUP exists ###
-  echo "Directory ${DIRBACKUP} exists"
+  echo -e "$(c g)Directory ${DIRBACKUP} exists"
 
 else
 
   ###  Control will jump here if $DIRBACKUP does NOT exists ###
-  echo "Directory ${DIRBACKUP} does not exist"
+  echo -e "$(c r)Directory ${DIRBACKUP} does not exist"
   mkdir -p $DIRBACKUP
-  echo "Directory ${DIRBACKUP} created"
+  echo -e "$(c g)Directory ${DIRBACKUP} created"
 
 fi
 
 if [ -d "$DIR" ]; then
   ### Take action if $DIR exists ###
-  echo "Directory ${DIR} exists"
+  echo -e "$(c g)Directory ${DIR} exists"
 
   if [ -z "$(ls -A -- "$DIR")" ]; then
 
     ### Take action if $DIR has NO content ###
-    echo "Directory ${DIR} IS EMPTY"
+    echo -e "$(c r)Directory ${DIR} IS EMPTY"
 
     if [ -z "$(ls -A -- "$DIRBACKUP")" ]; then
 
-      echo "Directory ${DIRBACKUP} IS EMPTY"
+      echo -e "$(c r)Directory ${DIRBACKUP} IS EMPTY"
       exit 1
 
     else
 
-      echo "Directory ${DIRBACKUP} has content"
+      echo -e "$(c g)Directory ${DIRBACKUP} has content"
       cd $DIRBACKUP
       $BACKUP = find $DIRBACKUP -mindepth 1 -maxdepth 1 -type d | sort -rn | head -1
       tar zxvf $BACKUP --directory $DIR
@@ -46,7 +46,7 @@ if [ -d "$DIR" ]; then
   else
 
     ###  Control will jump here if $DIR has content ###
-    echo "Directory ${DIR} has content"
+    echo -e "$(c g)Directory ${DIR} has content"
     cd $DIRBACKUP
     tar zcvf $DATETIME.tar.gz --absolute-names $DIR
     exit 1
@@ -57,16 +57,18 @@ if [ -d "$DIR" ]; then
 else
 
   ###  Control will jump here if $DIR does NOT exists ###
-  echo "Directory ${DIR} does not exist"
+  echo -e "$(c r)Directory ${DIR} does not exist"
   mkdir -p $DIR
-  echo "Directory ${DIR} created"
+  echo -e "$(c g)Directory ${DIR} created"
 
   if [ -z "$(ls -A -- "$DIRBACKUP")" ]; then
 
+    echo -e "$(c r)Directory ${DIRBACKUP} IS EMPTY"
     exit 1
 
   else
 
+    echo -e "$(c g)Directory ${DIRBACKUP} has content"
     cd $DIRBACKUP
     $BACKUP = find $DIRBACKUP -mindepth 1 -maxdepth 1 -type d | sort -rn | head -1
     tar zxvf $BACKUP --directory $DIR
