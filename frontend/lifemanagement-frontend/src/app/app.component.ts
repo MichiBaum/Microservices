9b4e55f66880f4654c12a24da716b7dc359c5830
MichiBaum <michael_baumberger@gmx.ch>
2020-07-06 10:44:08 +0200

    Lighthouse accept as PWA but offline not working yet

--- frontend/lifemanagement-frontend/src/app/app.component.ts
+++ frontend/lifemanagement-frontend/src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(private swupdate: SwUpdate) {
+    // checks if update available
+    this.swupdate.available.subscribe((event: any) => {
+      // reload / refresh the browser
+      this.swupdate.activateUpdate().then(() => document.location.reload());
+      console.log('sw updated');
+    });
+  }
+}
