586a5b39ad6e9bdda55b633868e5e107abdbb555
MichiBaum <michael_baumberger@gmx.ch>
2020-07-27 15:55:01 +0200

    only spaces

--- frontend/lifemanagement-frontend/src/app/navigation/navigation.component.ts
+++ frontend/lifemanagement-frontend/src/app/navigation/navigation.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {MenuItem} from 'primeng';
+import {environment} from '../../environments/environment';
 import {LanguageConfig} from '../core/language.config';
 import {PermissionEnum} from '../core/models/enum/permission.enum';
 import {AuthService} from '../core/services/auth.service';
 import {RouternavigationService} from '../core/services/routernavigation.service';
 import {LoginService} from '../login/login.service';
-import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'app-navigation',
@@ -40,6 +40,7 @@ export class NavigationComponent implements OnInit {
 
   setItems = (): void => {
     this.navItems = [
+
       {
         label: this.translate.instant('navigation.home'),
         icon: 'fas fa-house-user',
@@ -48,6 +49,7 @@ export class NavigationComponent implements OnInit {
           this.routernavigationService.homeNavigate();
         }
       } as MenuItem,
+
       {
         label: this.translate.instant('navigation.usersettings'),
         icon: 'fas fa-users-cog',
@@ -56,6 +58,7 @@ export class NavigationComponent implements OnInit {
           this.routernavigationService.usersettingsNavigate();
         }
       } as MenuItem,
+
       {
         label: this.translate.instant('navigation.logs'),
         icon: 'fas fa-cogs',
@@ -65,6 +68,7 @@ export class NavigationComponent implements OnInit {
         },
         visible: this.authService.hasAnyPermission([PermissionEnum.SEE_LOGS])
       } as MenuItem,
+
       {
         label: this.translate.instant('navigation.imprint'),
         icon: 'fas fa-stamp',
@@ -73,6 +77,7 @@ export class NavigationComponent implements OnInit {
           this.routernavigationService.imprintNavigate();
         }
       } as MenuItem,
+
       {
         label: this.translate.instant('navigation.privacy-policy'),
         icon: 'fas fa-user-shield',
@@ -81,6 +86,7 @@ export class NavigationComponent implements OnInit {
           this.routernavigationService.privacyPolicyNavigate();
         }
       } as MenuItem,
+
       {
         label: this.translate.instant('navigation.github'),
         icon: 'fab fa-github',
@@ -91,6 +97,7 @@ export class NavigationComponent implements OnInit {
         },
         visible: this.authService.hasAnyPermission([PermissionEnum.DEVELOP_TOOLS])
       } as MenuItem,
+
       {
         label: this.translate.instant('navigation.frontend_documentation'),
         icon: 'fab fa-github',
@@ -101,6 +108,7 @@ export class NavigationComponent implements OnInit {
         },
         visible: this.authService.hasAnyPermission([PermissionEnum.DEVELOP_TOOLS])
       } as MenuItem,
+
       {
         label: this.translate.instant('navigation.backend_api'),
         icon: 'fas fa-server',
@@ -111,6 +119,7 @@ export class NavigationComponent implements OnInit {
         },
         visible: this.authService.hasAnyPermission([PermissionEnum.DEVELOP_TOOLS])
       } as MenuItem,
+
       {
         label: this.translate.instant('navigation.logout'),
         icon: 'fas fa-sign-out-alt',
@@ -119,6 +128,7 @@ export class NavigationComponent implements OnInit {
           this.sidebarVisible = false;
         }
       } as MenuItem
+
     ] as MenuItem[];
   }
 
