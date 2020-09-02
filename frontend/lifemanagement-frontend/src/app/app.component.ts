546c7580229f477b272e9578073269262e950aeb
MichiBaum <michael_baumberger@gmx.ch>
2020-09-02 13:20:00 +0200

    Added primengConfig to enable ripple & added TODO to add ripple on elements

--- frontend/lifemanagement-frontend/src/app/app.component.ts
+++ frontend/lifemanagement-frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SwUpdate} from '@angular/service-worker';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +10,22 @@ import {SwUpdate} from '@angular/service-worker';
 /**
  * The App Component
  */
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   /**
    * Constructor which subscribes to the {@link SwUpdate} and updates
    * @param swupdate the {@link SwUpdate}
    */
-  constructor(private swupdate: SwUpdate) {
+  constructor(private swupdate: SwUpdate, private primengConfig: PrimeNGConfig) {
     // checks if update available
     this.swupdate.available.subscribe((event: any) => {
       // reload / refresh the browser
       this.swupdate.activateUpdate().then(() => document.location.reload());
     });
+  }
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true; // TODO add ripple to some elements
   }
 
 }
