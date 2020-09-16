2d0a13d00a9608563fb4f5c5ab6293e14cda8d18
MichiBaum <michael_baumberger@gmx.ch>
2020-09-16 11:26:24 +0200

    buggsixing & some config update

--- backend/monitoring/src/main/kotlin/com/michibaum/monitoring/MonitoringApplication.kt
+++ backend/monitoring/src/main/kotlin/com/michibaum/monitoring/MonitoringApplication.kt
@@ -2,10 +2,12 @@ package com.michibaum.monitoring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableDiscoveryClient
 class MonitoringApplication
 
 fun main(args: Array<String>) {
