4e1209462f0722a6526344c53b7643f0c1233b62
MichiBaum <michael_baumberger@gmx.ch>
2020-09-17 16:04:48 +0200

    Added spring admin things

--- backend/admin/src/main/kotlin/com/michibaum/admin/AdminApplication.kt
+++ backend/admin/src/main/kotlin/com/michibaum/admin/AdminApplication.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
-
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
