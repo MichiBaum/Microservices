a7f5575be66150b6910d7e53d2d76f71d44831ab
MichiBaum <michael_baumberger@gmx.ch>
2020-10-14 00:04:33 +0200

    Bugfix starting Admin Application

--- microservice/admin/src/main/java/com/michibaum/admin/AdminApplication.java
+++ microservice/admin/src/main/java/com/michibaum/admin/AdminApplication.java
@@ -2,9 +2,8 @@ package com.michibaum.admin;
 
 import brave.sampler.Sampler;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
@@ -18,10 +17,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class AdminApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder()
-                .main(AdminApplication.class)
-                .web(WebApplicationType.REACTIVE)
-                .run(args);
+        SpringApplication.run(AdminApplication.class, args);
     }
 
     @Bean
