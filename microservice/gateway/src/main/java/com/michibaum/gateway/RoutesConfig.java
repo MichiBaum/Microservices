db3ba3d515d92b45c2b82127d410ebed00d9dad1
Michael Baumberger <michael_baumberger@gmx.ch>
2021-05-27 04:32:46 +0200

    updated microservice

--- microservice/gateway/src/main/java/com/michibaum/gateway/RoutesConfig.java
+++ microservice/gateway/src/main/java/com/michibaum/gateway/RoutesConfig.java
@@ -20,31 +20,26 @@ public class RoutesConfig {
                                         .rewritePath("/services/admin", "")
                         )
                         .uri("lb://admin")
-                        .id("admin-service")
                 )
 
                 // Registry
                 .route(r -> r.path("/services/registry/**") //TODO doesnt work
                         .uri("lb://registry")
-                        .id("registry-service")
                 )
 
                 // Actuator Kibana
                 .route(r -> r.path("/kibana/actuator/**")
                         .uri("http://actuator-kibana:5601")
-                        .id("actuator-kibana")
                 )
 
                 // Zipkin Kibana
                 .route(r -> r.path("/kibana/zipkin/**")
                         .uri("http://zipkin-kibana:5601")
-                        .id("zipkin-kibana")
                 )
 
                 // Zipkin
                 .route(r -> r.path("/zipkin/**")
                         .uri("http://zipkin:9411")
-                        .id("zipkin")
                 )
 
                 .build();
