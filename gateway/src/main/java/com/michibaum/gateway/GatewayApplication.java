3aceaf73c74550af244dcf627f2d9936f3c93b99
Michael Baumberger <michael_baumberger@gmx.ch>
2021-06-11 23:52:19 +0200

    Changed Gateway application

--- gateway/src/main/java/com/michibaum/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/michibaum/gateway/GatewayApplication.java
@@ -3,13 +3,26 @@ package com.michibaum.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Configuration
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator(ReactiveDiscoveryClient discoveryClient, DiscoveryLocatorProperties properties) {
+		properties.setEnabled(true);
+		properties.setLowerCaseServiceId(true);
+		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
 	}
 
 }
