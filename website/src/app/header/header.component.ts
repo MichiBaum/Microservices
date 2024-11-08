5e80e0777e9e8c2b0d5c90a88f2f5cd98fee5e6d
Michael Baumberger <michael_baumberger@gmx.ch>
2024-11-08 16:13:37 +0100

    Add language select component
    Extract the language select functionality from the header component into a dedicated `LanguageSelectComponent`. This improves modularity and separates concerns, making the code easier to maintain and extend.

--- website/src/app/header/header.component.ts
+++ website/src/app/header/header.component.ts
@@ -4,7 +4,6 @@ import {TranslateModule, TranslateService} from "@ngx-translate/core";
 import {PrimeTemplate} from "primeng/api";
 import {DropdownModule} from "primeng/dropdown";
 import {FormsModule} from "@angular/forms";
-import {PrimeNgBase} from "../core/models/primeng-base.model";
 import {LanguageConfig} from "../core/config/language.config";
 import {HeaderService} from "../core/services/header.service";
 import {Title} from "@angular/platform-browser";
@@ -27,9 +26,6 @@ import {LogoutComponent} from "../logout/logout.component";
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit{
-
-  languages: PrimeNgBase[] | undefined;
-
   title = "application.title"
 
   constructor(private languageConfig: LanguageConfig, private translate: TranslateService, private headerService: HeaderService, private titleService: Title) {
@@ -37,7 +33,6 @@ export class HeaderComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    this.languages = this.initLanguages();
     this.headerService.titleChangeEmitter.subscribe(value => {
       this.changeTitle(value)
     })
@@ -51,17 +46,5 @@ export class HeaderComponent implements OnInit{
     this.titleService.setTitle(this.translate.instant(title))
   }
 
-  initLanguages = () => {
-    return this.languageConfig.languages.map((lang) => {
-      return {label: this.translate.instant(lang.name), field: lang.isoCode, value: lang.isoCode} as PrimeNgBase;
-    });
-  }
-
-  selectLanguage = (event: any) => {
-    const language = this.languageConfig.languages.find((lang) => lang.isoCode === event.value);
-    if(language == undefined)
-      return;
-    this.languageConfig.setLanguage(language);
-  }
 
 }
