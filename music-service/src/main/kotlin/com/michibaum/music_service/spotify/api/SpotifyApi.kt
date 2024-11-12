7898b4a06fd32c74d88b4bfa2a865eb56cf72f0d
Michael Baumberger <michael_baumberger@gmx.ch>
2024-11-13 00:42:16 +0100

    Remove SpotifyProfileApi from SpotifyApi interface
    This change focuses on streamlining the SpotifyApi interface by removing the inheritance from SpotifyProfileApi. The interface now solely extends SpotifyUserApi, which simplifies its function and reduces unnecessary dependencies.

--- music-service/src/main/kotlin/com/michibaum/music_service/spotify/api/SpotifyApi.kt
+++ music-service/src/main/kotlin/com/michibaum/music_service/spotify/api/SpotifyApi.kt
@@ -1,7 +1,6 @@
 package com.michibaum.music_service.spotify.api
 
-import com.michibaum.music_service.spotify.api.profile.SpotifyProfileApi
 import com.michibaum.music_service.spotify.api.user.SpotifyUserApi
 
-interface SpotifyApi: SpotifyProfileApi, SpotifyUserApi {
+interface SpotifyApi: SpotifyUserApi {
 }
