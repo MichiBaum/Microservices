f183064493fdc31262d7e11f9eb1b092b5ac72fa
Michael Baumberger <michael_baumberger@gmx.ch>
2024-11-16 23:51:02 +0100

    Add Fitbit subscription and notification handling
    Introduced entities for Fitbit notifications and subscriptions including related service and repository. Implemented REST controller for handling Fitbit notification API with conversion logic for DTOs to domain objects.

---
+++ fitness-service/src/main/kotlin/com/michibaum/fitness_service/fitbit/subscriptions/FitbitSubscriptionRepository.kt
@@ -0,0 +1,8 @@
+package com.michibaum.fitness_service.fitbit.subscriptions
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface FitbitSubscriptionRepository: JpaRepository<FitbitSubscription, UUID> {
+
+}
