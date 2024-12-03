95eaf0857be0d5a36b968d57a91f0900dd2aabc7
Michael Baumberger <michael_baumberger@gmx.ch>
2024-12-03 14:03:21 +0100

    Add Lichess PGN Viewer for displaying chess games
    Integrated the Lichess PGN Viewer into the chess event games component to visually display chess game data. This enhancement involves updating the component's logic and template to include a button that opens a dialog displaying the game when clicked. Additionally, necessary dependencies have been added to the project to support this feature.

--- website/src/app/chess/chess-event/chess-event-games/chess-event-games.component.ts
+++ website/src/app/chess/chess-event/chess-event-games/chess-event-games.component.ts
@@ -1,11 +1,20 @@
-import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
+import {EventEmitter, Output, Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild} from '@angular/core';
 import {ChessEvent, ChessGame} from "../../../core/models/chess/chess.models";
 import {ChessService} from "../../../core/services/chess.service";
+import LichessPgnViewer from 'lichess-pgn-viewer';
+import PgnViewer from "lichess-pgn-viewer/pgnViewer";
+import {Button} from "primeng/button";
+import {DialogModule} from "primeng/dialog";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-chess-event-games',
   standalone: true,
-  imports: [],
+  imports: [
+    Button,
+    DialogModule,
+    NgForOf
+  ],
   templateUrl: './chess-event-games.component.html',
   styleUrl: './chess-event-games.component.scss'
 })
@@ -14,9 +23,14 @@ export class ChessEventGamesComponent implements OnChanges {
   games: ChessGame[] | undefined;
   @Output() haveContent = new EventEmitter<boolean>();
 
+  @ViewChild('game',{static:false})
+  gameDiv: ElementRef | undefined;
+
+  lpv: PgnViewer | undefined;
+
   constructor(
     private readonly chessService: ChessService,
-  ) {}
+  ) { }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['event'] && changes['event'].currentValue) {
@@ -25,6 +39,24 @@ export class ChessEventGamesComponent implements OnChanges {
         this.haveContent.emit(this.games.length > 0)
       })
     }
+  }
+
+  visible: boolean = false;
+
+  showDialog(game: ChessGame) {
+    if(game.pgn){
+      this.visible = true;
+      setInterval(() => {
+        this.initGame(game.pgn);
+      }, 500);
+    }
+  }
+
+  initGame(pgn: string): void {
+    this.lpv = LichessPgnViewer(this.gameDiv?.nativeElement, {
+      pgn: pgn,
+    });
+
   }
 
 }
