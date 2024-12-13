abb7bc7f115b8a2e14cbb8be78fc449efd716dc3
Michael Baumberger <michael_baumberger@gmx.ch>
2024-12-13 17:23:36 +0100

    Enable advanced filtering for chess events
    Added custom filters for event categories and URLs in the chess event selection component. Updated the UI to include new filters and ensured proper initialization during component setup. This improves event searching and usability in the settings.

--- website/src/app/chess-settings/select-chess-event/select-chess-event.component.ts
+++ website/src/app/chess-settings/select-chess-event/select-chess-event.component.ts
@@ -1,12 +1,13 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {TableModule} from "primeng/table";
-import {ChessEvent} from "../../core/models/chess/chess.models";
+import {ChessEvent, ChessEventCategory} from "../../core/models/chess/chess.models";
 import {InputTextModule} from "primeng/inputtext";
 import {FormsModule} from "@angular/forms";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {faCheck, faXmark} from "@fortawesome/free-solid-svg-icons";
 import {NgForOf} from "@angular/common";
 import {ConfirmDialogModule} from "primeng/confirmdialog";
+import {FilterService, SelectItem} from "primeng/api";
 
 @Component({
   selector: 'app-select-chess-event',
@@ -22,7 +23,7 @@ import {ConfirmDialogModule} from "primeng/confirmdialog";
   templateUrl: './select-chess-event.component.html',
   styleUrl: './select-chess-event.component.scss'
 })
-export class SelectChessEventComponent {
+export class SelectChessEventComponent implements OnInit{
 
   @Input()
   events: ChessEvent[] = [];
@@ -32,10 +33,40 @@ export class SelectChessEventComponent {
 
   selectedEvent: ChessEvent | undefined;
   tableSearch: string = "";
+  matchModeOptions: SelectItem[] = [];
+  eventCategoryFilterName = 'anyEventCategoryLike';
+  eventUrlFilterName = 'eventUrlPresent';
 
   constructor(
-
+    private readonly filterService: FilterService
   ) { }
+
+  ngOnInit(): void {
+    this.filterService.register(this.eventCategoryFilterName, (value: any, filter:any): boolean => {
+      if (filter === undefined || filter === null || filter.trim() === '') { return true; }
+      if (value === undefined || value === null) { return false; }
+
+      const eventCategories = value as ChessEventCategory[];
+      const filterText = filter as string;
+      return eventCategories.some(value1 => value1.title.toLowerCase().includes(filterText.toLowerCase()))
+    });
+    this.filterService.register(this.eventUrlFilterName, (value: any, filter:any): boolean => {
+      if (filter === undefined || filter === null) { return true; }
+
+      const valueUndefined = value === undefined || value === null || value === '';
+      if(filter && !valueUndefined){
+        return true;
+      }
+      if (!filter && valueUndefined) {
+        return true;
+      }
+      return false;
+    });
+    this.matchModeOptions = [
+      { label: 'Any Event Category Like', value: this.eventCategoryFilterName },
+      { label: 'Event Url Present', value: this.eventUrlFilterName }
+    ];
+  }
 
   onSelectionChange() {
     this.selectedEventEmitter.emit(this.selectedEvent)
