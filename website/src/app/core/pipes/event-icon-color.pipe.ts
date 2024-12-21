7677b6f6aaf5973718603f98433ed77a2aaf118e
Michael Baumberger <michael_baumberger@gmx.ch>
2024-12-21 18:07:47 +0100

    Fix date comparison logic in event-icon-color pipe
    Updated date comparisons to ignore time components by setting hours, minutes, and seconds to zero. This ensures correct color assignment for ongoing events, including same-day scenarios.

--- website/src/app/core/pipes/event-icon-color.pipe.ts
+++ website/src/app/core/pipes/event-icon-color.pipe.ts
@@ -9,10 +9,10 @@ export class EventIconColorPipe implements PipeTransform {
 
   transform(event: ChessEvent): string {
     if(event.dateFrom && event.dateTo){
-      const dateFrom = new Date(event.dateFrom)
-      const dateTo = new Date(event.dateTo)
-      const current = new Date()
-      if(dateTo > current && dateFrom < current){ return "color: green"}
+      const dateFrom = new Date(event.dateFrom).setHours(0,0,0,0)
+      const dateTo = new Date(event.dateTo).setHours(0,0,0,0)
+      const current = new Date().setHours(0,0,0,0)
+      if(dateTo >= current && dateFrom <= current){ return "color: green"}
       if(dateTo < current ){ return "color: red"}
       if(dateFrom > current){ return "color: #0688fb"}
     }
