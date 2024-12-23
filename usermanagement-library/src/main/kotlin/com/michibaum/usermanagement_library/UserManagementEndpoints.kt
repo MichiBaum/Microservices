ac74a0fd16a8fdd5252261e952c53f878bfb8049
Michael Baumberger <michael_baumberger@gmx.ch>
2024-12-23 21:56:21 +0100

    Fix: Remove unused imports and @Valid annotations from endpoints
    Unused imports were cleaned to reduce clutter and improve readability. The @Valid annotations were removed as validation logic will be handled elsewhere, ensuring clearer separation of concerns.

--- usermanagement-library/src/main/kotlin/com/michibaum/usermanagement_library/UserManagementEndpoints.kt
+++ usermanagement-library/src/main/kotlin/com/michibaum/usermanagement_library/UserManagementEndpoints.kt
@@ -1,17 +1,14 @@
 package com.michibaum.usermanagement_library
 
-import jakarta.validation.Valid
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 
 interface UserManagementEndpoints {
 
     @PostMapping(value = ["/api/checkUserDetails"])
-    fun checkUserDetails(@RequestBody @Valid loginDto: LoginDto): UserDetailsDto?
+    fun checkUserDetails(@RequestBody loginDto: LoginDto): UserDetailsDto?
 
     @PostMapping("/api/users")
-    fun create(@RequestBody @Valid createUserDto: CreateUserDto): UserDetailsDto?
+    fun create(@RequestBody createUserDto: CreateUserDto): UserDetailsDto?
 
 }
