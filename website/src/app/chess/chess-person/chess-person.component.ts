1c36a976e7674c5b8763cf0007fcc1f1fad57233
Michael Baumberger <michael_baumberger@gmx.ch>
2024-12-24 15:56:04 +0100

    Add person details endpoint and UI component
    Introduced a new API endpoint to fetch person details by ID in the backend and added corresponding routes, service methods, and a new Angular component (`ChessPersonComponent`) to display person details in the frontend. Included unit tests for the new component to ensure functionality.

---
+++ website/src/app/chess/chess-person/chess-person.component.ts
@@ -0,0 +1,36 @@
+import {Component, inject, OnDestroy, signal, WritableSignal} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {ChessService} from "../../core/services/chess.service";
+import {rxResource} from "@angular/core/rxjs-interop";
+import {EMPTY} from "rxjs";
+
+@Component({
+  selector: 'app-chess-person',
+  imports: [],
+  templateUrl: './chess-person.component.html',
+  styleUrl: './chess-person.component.scss'
+})
+export class ChessPersonComponent implements OnDestroy {
+  private readonly route = inject(ActivatedRoute);
+  private readonly chessService = inject(ChessService);
+
+  personId: WritableSignal<string | undefined> = signal("")
+  person = rxResource({
+    request: () => ({id: this.personId()}),
+    loader: (param) => {
+      if(param.request.id == undefined)
+        return EMPTY
+      return this.chessService.person(param.request.id)
+    }
+  })
+
+  routeParamsSubscription = this.route.params.subscribe(params => {
+    const id = params['id'];
+    this.personId.set(id)
+  });
+
+  ngOnDestroy() {
+    this.routeParamsSubscription.unsubscribe()
+  }
+
+}
