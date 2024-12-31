292dd56a5495ce5b2f689927b4e508bf4a0391ee
Michael Baumberger <michael_baumberger@gmx.ch>
2024-12-31 14:44:14 +0100

    Add additional `info` properties to all application.yml files
    Enable `build`, `defaults`, `env`, `java`, `os`, `process`, and `ssl` under the `info` section across multiple services’ configuration files. This enhances visibility of system and build metadata in the various service configurations. Also updates the authentication manager to handle multiple authentications properly.

--- authentication-library/src/main/kotlin/com/michibaum/authentication_library/security/ServletDelegateAuthenticationManager.kt
+++ authentication-library/src/main/kotlin/com/michibaum/authentication_library/security/ServletDelegateAuthenticationManager.kt
@@ -1,6 +1,7 @@
 package com.michibaum.authentication_library.security
 
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 
@@ -10,14 +11,21 @@ class ServletDelegateAuthenticationManager(private val authenticationManagers: L
             throw Exception("Empty authentication")
         }
 
+        val auths = mutableListOf<Authentication>()
         for(authManager in authenticationManagers){
             if(authManager.supports(authentication.javaClass)){
-                val auth =  authManager.authenticate(authentication) ?: throw Exception("Empty authentication")
-                SecurityContextHolder.getContext().authentication = auth
-                return auth
+                val auth = authManager.authenticate(authentication) ?: continue
+                auths.add(auth)
             }
         }
 
-        throw NoAuthenticationManagerException("No authentication Manager found for ${authentication::class}")
+        val authenticated = auths.filter { it.isAuthenticated }
+
+        if(authenticated.size > 1 || authenticated.isEmpty()) {
+            throw BadCredentialsException("More than one, or none authentication was authenticated.")
+        }
+
+        SecurityContextHolder.getContext().authentication = authenticated[0]
+        return authenticated[0]
     }
 }
