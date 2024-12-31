5b844e5c335917c71b4191f3cf9790c33bd5c82c
Michael Baumberger <michael_baumberger@gmx.ch>
2024-12-31 02:06:42 +0100

    Add Flyway-based database cleanup for tests
    Introduce `FlywayClearDatabaseJunitExtension` to clean and migrate databases before each test across all services. Replace `DirtiesContext` with this extension in `TestcontainersConfiguration` and ensure proper Flyway configuration with test properties. This enhances test isolation and improves database handling consistency.

--- authentication-service/src/test/kotlin/com/michibaum/authentication_service/TestcontainersConfiguration.kt
+++ authentication-service/src/test/kotlin/com/michibaum/authentication_service/TestcontainersConfiguration.kt
@@ -1,10 +1,12 @@
 package com.michibaum.authentication_service
 
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.context.annotation.Import
-import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.TestPropertySource
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ExtendWith(value = [FlywayClearDatabaseJunitExtension::class])
 @Import(value = [TestcontainersTestConfiguration::class])
+@TestPropertySource(properties = ["spring.flyway.clean-disabled=false"])
 annotation class TestcontainersConfiguration()
--- fitness-service/src/test/kotlin/com/michibaum/fitness_service/TestcontainersConfiguration.kt
+++ fitness-service/src/test/kotlin/com/michibaum/fitness_service/TestcontainersConfiguration.kt
@@ -1,13 +1,16 @@
 package com.michibaum.fitness_service
 
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.context.annotation.Import
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.annotation.DirtiesContext.ClassMode.*
+import org.springframework.test.context.TestPropertySource
 import kotlin.annotation.AnnotationRetention.*
 import kotlin.annotation.AnnotationTarget.*
 
-@Target(CLASS)
-@Retention(RUNTIME)
-@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@ExtendWith(value = [FlywayClearDatabaseJunitExtension::class])
 @Import(value = [TestcontainersTestConfiguration::class])
+@TestPropertySource(properties = ["spring.flyway.clean-disabled=false"])
 annotation class TestcontainersConfiguration()
