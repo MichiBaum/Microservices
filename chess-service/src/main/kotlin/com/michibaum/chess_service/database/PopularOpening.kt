1043a770e9634a9b10a55ecc917b24a9a8ab7def
Michael Baumberger <michael_baumberger@gmx.ch>
2025-02-02 23:01:07 +0100

    Add PopularOpening entity and database table
    Introduces the `PopularOpening` entity to represent popular chess openings, linked to the `Opening` entity. Also adds a corresponding database migration to create the `popular_opening` table with a foreign key to the `opening` table. This sets the foundation for tracking popular chess openings effectively.

---
+++ chess-service/src/main/kotlin/com/michibaum/chess_service/database/PopularOpening.kt
@@ -0,0 +1,20 @@
+package com.michibaum.chess_service.database
+
+import jakarta.persistence.*
+import java.util.*
+
+@Entity
+@Table(name = "popular_opening")
+data class PopularOpening(
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = [CascadeType.ALL], optional = false)
+    @JoinColumn(name = "opening_id")
+    val opening: Opening,
+
+    @Column(nullable = false)
+    val ranking: Int = 0,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID? = null,
+)
