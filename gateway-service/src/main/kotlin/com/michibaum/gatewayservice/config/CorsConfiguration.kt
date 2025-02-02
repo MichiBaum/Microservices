9dd22196cdd1a25e25eef4c0496535566faf041c
Michael Baumberger <michael_baumberger@gmx.ch>
2025-02-03 00:26:43 +0100

    BUGFIX: Refactor CORS configuration to use externalized properties
    Replaced hardcoded CORS configurations with a properties-based approach using `CorsProperties`. This centralizes management of allowed origins in application configuration files and improves flexibility for different environments. Updated both development and production YAML files to reflect the new structure.

--- gateway-service/src/main/kotlin/com/michibaum/gatewayservice/config/CorsConfiguration.kt
+++ gateway-service/src/main/kotlin/com/michibaum/gatewayservice/config/CorsConfiguration.kt
@@ -1,5 +1,7 @@
 package com.michibaum.gatewayservice.config
 
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
@@ -7,15 +9,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebMvc
-@Profile("dev")
-class CorsConfiguration: WebMvcConfigurer {
+@EnableConfigurationProperties(value = [CorsProperties::class])
+class CorsConfiguration(
+    private val corsProperties: CorsProperties
+): WebMvcConfigurer {
 
     override fun addCorsMappings(registry: org.springframework.web.servlet.config.annotation.CorsRegistry) {
         registry.addMapping("/**")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-            .allowedOrigins("http://michibaum.ch:4200", "http://michibaum.com:4200", "http://michibaum.eu:4200")
+            .allowedOriginPatterns(*corsProperties.allowedOriginPatterns.toTypedArray())
             .allowedHeaders("*")
             .allowCredentials(true)
     }
 
 }
+
+@ConfigurationProperties(prefix = "cors")
+data class CorsProperties(
+    val allowedOriginPatterns: List<String>
+)
