e26b9febf18b6bb3a80a633d1e13b40795dee15b
Michael Baumberger <michael_baumberger@gmx.ch>
2025-02-07 02:59:48 +0100

    Update Zipkin configuration and standardize logging pattern
    Added Zipkin configuration properties to the gateway service and refactored route configuration to use these properties. Updated logging patterns across all services to include traceId for improved traceability and standardized timestamps to `yyyy-MM-dd HH:mm:ss` format. These changes enhance log consistency and facilitate distributed tracing.

--- gateway-service/src/main/kotlin/com/michibaum/gatewayservice/config/RouterConfiguration.kt
+++ gateway-service/src/main/kotlin/com/michibaum/gatewayservice/config/RouterConfiguration.kt
@@ -2,6 +2,8 @@ package com.michibaum.gatewayservice.config
 
 import com.michibaum.gatewayservice.app.robotstxt.RobotsTxtController
 import com.michibaum.gatewayservice.app.sitemapxml.SitemapXmlController
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http
@@ -13,6 +15,7 @@ import org.springframework.web.servlet.function.ServerResponse
 import java.net.URI
 
 @Configuration
+@EnableConfigurationProperties(value = [ZipkinProperties::class])
 class RouterConfiguration {
 
     /**
@@ -20,6 +23,7 @@ class RouterConfiguration {
      */
     @Bean
     fun gatewayRoutes(
+        zipkinProperties: ZipkinProperties,
         robotsTxtController: RobotsTxtController,
         sitemapXmlController: SitemapXmlController
     ): RouterFunction<ServerResponse> {
@@ -44,8 +48,9 @@ class RouterConfiguration {
 
             // Specific services (Higher priority)
             .route(host("admin.michibaum.*"), lb("admin-service").apply(http()))
-            .route(host("zipkin.michibaum.*"), http("http://localhost:9411"))
-            .route(host("registry.michibaum.*"), lb("registry-service").apply(http()))
+            // TODO add authentication
+            .route(host("zipkin.michibaum.*"), http(zipkinProperties.serviceUrl))
+//            .route(host("registry.michibaum.*"), lb("registry-service").apply(http()))
             .route(host("authentication.michibaum.*"), lb("authentication-service").apply(http()))
             .route(host("usermanagement.michibaum.*"), lb("usermanagement-service").apply(http()))
             .route(host("chess.michibaum.*"), lb("chess-service").apply(http()))
@@ -58,3 +63,8 @@ class RouterConfiguration {
     }
 
 }
+
+@ConfigurationProperties(prefix = "management.zipkin")
+data class ZipkinProperties(
+    val serviceUrl: URI
+)
