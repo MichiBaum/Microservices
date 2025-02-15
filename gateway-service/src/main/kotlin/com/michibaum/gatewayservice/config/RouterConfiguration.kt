b36f227e51f8fd579b0fc804b27a415eaece9f3e
Michael Baumberger <michael_baumberger@gmx.ch>
2025-02-15 17:13:56 +0100

    Enable routing for the registry-service in RouterConfiguration.
    This commit removes the commented-out block and activates routing for the registry.michibaum.* domain to the registry-service. This change enables the registry-service to properly handle incoming traffic.

--- gateway-service/src/main/kotlin/com/michibaum/gatewayservice/config/RouterConfiguration.kt
+++ gateway-service/src/main/kotlin/com/michibaum/gatewayservice/config/RouterConfiguration.kt
@@ -26,8 +26,7 @@ class RouterConfiguration {
 
             // Specific services (Higher priority)
             .route(host("admin.michibaum.*"), lb("admin-service").apply(http()))
-            // TODO Authentication
-            //  .route(host("registry.michibaum.*"), lb("registry-service").apply(http()))
+            .route(host("registry.michibaum.*"), lb("registry-service").apply(http()))
             .route(host("authentication.michibaum.*"), lb("authentication-service").apply(http()))
             .route(host("usermanagement.michibaum.*"), lb("usermanagement-service").apply(http()))
             .route(host("chess.michibaum.*"), lb("chess-service").apply(http()))
