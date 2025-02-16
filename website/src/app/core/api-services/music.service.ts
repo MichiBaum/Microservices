6cbaca20a8bf3e97357ade595581db37b364dae8
Michael Baumberger <michael_baumberger@gmx.ch>
2025-02-16 23:49:07 +0100

    Improve music module with new settings page and navigation link This commit introduces a settings page to the music module, accessible through the `/music/settings` path. A new link has been added to the music navigation menu for easy access to this settings page. Minor cleanups like adjusting the imports and updating a TODO comment in the Spotify OAuth controller are also included in these changes. Simplified music components and enhanced navigation structure The music components have been reorganized into a more intuitive structure.

--- website/src/app/core/api-services/music.service.ts
+++ website/src/app/core/api-services/music.service.ts
@@ -3,7 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {HttpErrorHandler} from "../config/http-error-handler.service";
 import {UserInfoService} from "../services/user-info.service";
 import {Token} from "../models/music/token.model";
-import {catchError} from "rxjs";
+import {catchError, Observable} from "rxjs";
 import {EnvironmentConfig} from "../config/environment.config";
 
 @Injectable({providedIn: 'root'})
@@ -13,9 +13,9 @@ export class MusicService{
   private httpErrorConfig = inject(HttpErrorHandler);
   private userInfoService = inject(UserInfoService);
 
-
-  getToken() {
+  getToken(): Observable<Token> {
     return this.http.get<Token>(this.environment.musicService() + '/spotify/token')
       .pipe(catchError(err => this.httpErrorConfig.handleError(err, this.userInfoService)));
   }
+
 }
---
+++ website/src/app/music/music-spotify-login/music-spotify-login.component.ts
@@ -0,0 +1,22 @@
+import {Component, inject} from '@angular/core';
+import {Button} from "primeng/button";
+import {RouterNavigationService} from "../../core/services/router-navigation.service";
+import {MusicService} from "../../core/api-services/music.service";
+
+@Component({
+  selector: 'app-music-spotify-login',
+  imports: [
+    Button
+  ],
+  templateUrl: './music-spotify-login.component.html',
+  styleUrl: './music-spotify-login.component.scss'
+})
+export class MusicSpotifyLoginComponent {
+  private readonly musicService = inject(MusicService);
+  private readonly router = inject(RouterNavigationService);
+
+  getTokenUrl(){
+    this.musicService.getToken().subscribe(token => this.router.openPopup(token.url.replace(/ /g, "")));
+  }
+
+}
