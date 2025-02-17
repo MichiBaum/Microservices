813f39bcab000fe795163c3476ecf2be392b2288
Michael Baumberger <michael_baumberger@gmx.ch>
2025-02-17 19:11:44 +0100

    Add Musixmatch API integration and music entity management
    Implemented Musixmatch API support with DTOs, client classes, and configuration. Added database entities (Track, Album, Artist) and their repositories with mapping tables for relationships. Included new API keys and related properties in configuration files.

---
+++ music-service/src/main/kotlin/com/michibaum/music_service/database/Track.kt
@@ -0,0 +1,34 @@
+package com.michibaum.music_service.database
+
+import jakarta.persistence.*
+import java.time.LocalDate
+import java.util.*
+
+@Entity
+@Table(name="track")
+data class Track (
+
+    @Column(nullable = false)
+    val name: String,
+
+    @ManyToMany(fetch = FetchType.LAZY, targetEntity = Artist::class)
+    @JoinTable(name = "TRACK_ARTIST_MAPPING", joinColumns = [JoinColumn(name = "track_id")], inverseJoinColumns = [JoinColumn(name = "artist_id")])
+    val artists: List<Artist>,
+
+    @Column(nullable = true)
+    @Temporal(TemporalType.DATE)
+    val releaseDate: LocalDate,
+
+    @Column(nullable = false, unique = true)
+    val isrc: String, // International Standard Recording Code
+
+    @Column(nullable = true)
+    val ean: String, // International Article Number
+
+    @Column(nullable = true)
+    val upc: String, // Universal Product Code
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID? = null
+)
