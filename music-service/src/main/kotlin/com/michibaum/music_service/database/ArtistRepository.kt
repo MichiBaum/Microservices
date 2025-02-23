f6ba36161aa2b2cc83c7cca345019c772d5fee1f
Michael Baumberger <michael_baumberger@gmx.ch>
2025-02-23 17:53:16 +0100

    Add artist search and retrieval endpoints
    Introduce `ArtistController` to handle artist search and retrieval via REST endpoints. Add `ArtistService`, `ArtistConverter`, and `ArtistDto` to support these operations. Extend `ArtistRepository` with a case-insensitive search method by name.

--- music-service/src/main/kotlin/com/michibaum/music_service/database/ArtistRepository.kt
+++ music-service/src/main/kotlin/com/michibaum/music_service/database/ArtistRepository.kt
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface ArtistRepository: JpaRepository<Artist, UUID> {
+    fun findByNameContainingIgnoreCase(name: String): List<Artist>
 }
