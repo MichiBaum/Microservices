2adfe7540c9ca0ed88192f492515935b70e56bdb
Michael Baumberger <michael_baumberger@gmx.ch>
2025-02-27 20:28:17 +0100

    Remove EventVenue and unused gameType field; enhance entities.
    EventVenue entity and its enum were removed due to lack of use. Added event relationships to EventSponsor and EventMediaLink for improved data modeling. Made the gameType field redundant in Event and removed it to simplify the schema.

--- chess-service/src/main/kotlin/com/michibaum/chess_service/database/Event.kt
+++ chess-service/src/main/kotlin/com/michibaum/chess_service/database/Event.kt
@@ -35,10 +35,6 @@ class Event(
     @Column(nullable = false)
     val platform: ChessPlatform,
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    val gameType: GameType,
-
     @ManyToMany(fetch = FetchType.LAZY, targetEntity = EventCategory::class)
     @JoinTable(name = "EVENT_CATEGORY_MAPPING", joinColumns = [JoinColumn(name = "event_id")], inverseJoinColumns = [JoinColumn(name = "category_id")])
     val categories: Set<EventCategory> = setOf(),
