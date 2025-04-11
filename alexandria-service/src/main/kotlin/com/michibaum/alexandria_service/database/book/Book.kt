df7272e91345ac18d283b00bb805d48bdbf2a2cb
Michael Baumberger <michael_baumberger@gmx.ch>
2025-04-11 07:00:35 +0200

    Add entities, repositories, and controllers for database models
    This commit introduces entities for `Author`, `Book`, `Magazine`, `NewsArticle`, `NewsPaper`, `NewsTag`, `NewsCategory`, `Note`, and `ListOfContents`, along with their corresponding JPA repositories. Additionally, it creates basic controllers for `Author`, `Book`, `Magazine`, `News`, and `Note`. An initial database migration script is also added to define the `magazine` table.

--- alexandria-service/src/main/kotlin/com/michibaum/alexandria_service/database/book/Book.kt
+++ alexandria-service/src/main/kotlin/com/michibaum/alexandria_service/database/book/Book.kt
@@ -1,4 +1,14 @@
 package com.michibaum.alexandria_service.database.book
 
-class Book {
-}
+import jakarta.persistence.*
+import java.util.*
+
+@Entity
+@Table(name="book")
+class Book (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID? = null
+
+){}
