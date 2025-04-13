46d9393fc7308af985b493f22438241ec9cfbd92
Michael Baumberger <michael_baumberger@gmx.ch>
2025-04-13 11:46:51 +0200

    Add relationships for Author and ListOfContents entities
    Updated database models to include new relationships: Author is now linked to NewsArticle, Book, and other entities; ListOfContents is referenced in Book and Magazine. Adjusted table names, application configurations, and schema creation to align with the changes.

--- alexandria-service/src/main/kotlin/com/michibaum/alexandria_service/database/news/NewsArticle.kt
+++ alexandria-service/src/main/kotlin/com/michibaum/alexandria_service/database/news/NewsArticle.kt
@@ -1,11 +1,15 @@
 package com.michibaum.alexandria_service.database.news
 
+import com.michibaum.alexandria_service.database.Author
 import jakarta.persistence.*
 import java.util.*
 
 @Entity
 @Table(name="news_article")
 class NewsArticle (
+
+    @ManyToOne(targetEntity = Author::class, fetch = FetchType.LAZY, optional = false)
+    val author: Author,
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
