06938f17fd593a5a2dc88bc93b99beca3e3b1d70
Michael Baumberger <michael_baumberger@gmx.ch>
2025-06-03 14:54:54 +0200

    Refactor database relationships to replace many-to-many mappings with explicit join entities for improved design flexibility and clarity.

--- usermanagement-service/src/main/kotlin/com/michibaum/usermanagement_service/User.kt
+++ usermanagement-service/src/main/kotlin/com/michibaum/usermanagement_service/User.kt
@@ -15,10 +15,6 @@ class User (
     @Column(nullable = false)
     val password: String,
 
-    @ManyToMany(fetch = EAGER, targetEntity = Permission::class)
-    @JoinTable(name = "USER_PERMISSION_MAPPING", joinColumns = [JoinColumn(name = "user_id")], inverseJoinColumns = [JoinColumn(name = "permission_id")])
-    val permissions: Set<Permission>,
-
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     val id: UUID? = null,
