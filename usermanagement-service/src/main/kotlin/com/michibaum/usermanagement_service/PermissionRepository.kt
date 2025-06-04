cf067d0f22a898fcaf6ffe1d383ba98252767b44
Michael Baumberger <michael_baumberger@gmx.ch>
2025-06-04 08:51:25 +0200

    Refactor package structure for user management service; update UUID usage, adjust repositories, and improve default permission handling.

--- usermanagement-service/src/main/kotlin/com/michibaum/usermanagement_service/PermissionRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.michibaum.usermanagement_service
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PermissionRepository: JpaRepository<Permission, String> {
-}
