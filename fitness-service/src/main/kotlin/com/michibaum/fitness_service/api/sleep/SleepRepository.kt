a2545a707c3016cef18adfbffd52252a88c144e2
Michael Baumberger <michael_baumberger@gmx.ch>
2025-06-08 10:35:39 +0200

    Refactor sleep management: remove Fitbit-specific implementations, introduce support for sleep stage retrieval, update DTOs and repositories, refine API endpoints, and adjust frontend to handle the new sleep stage data model.

--- fitness-service/src/main/kotlin/com/michibaum/fitness_service/api/sleep/SleepRepository.kt
+++ fitness-service/src/main/kotlin/com/michibaum/fitness_service/api/sleep/SleepRepository.kt
@@ -2,9 +2,13 @@ package com.michibaum.fitness_service.api.sleep
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 @Repository
 interface SleepRepository: JpaRepository<Sleep, UUID> {
+    
+    @Query("SELECT s FROM Sleep s WHERE s.userId = :userId")
     fun findByUserId(userId: String): Set<Sleep>
+    
 }
--- website/src/app/core/api-services/fitness.service.ts
+++ website/src/app/core/api-services/fitness.service.ts
@@ -6,7 +6,7 @@ import {Weight} from "../models/fitness/weight.model";
 import {Profile} from "../models/fitness/profile.model";
 import {HttpErrorHandler} from "../config/http-error-handler.service";
 import {UserInfoService} from "../services/user-info.service";
-import {Sleep} from "../models/fitness/sleep.model";
+import {Sleep, SleepStage} from "../models/fitness/sleep.model";
 import {EnvironmentConfig} from "../config/environment.config";
 
 @Injectable({providedIn: 'root'})
@@ -33,7 +33,12 @@ export class FitnessService {
   }
 
   getSleep(): Observable<Sleep[]> {
-    return this.http.get<Sleep[]>(this.environment.fitnessService() + '/sleep')
+    return this.http.get<Sleep[]>(this.environment.fitnessService() + '/sleeps')
       .pipe(catchError(err => this.httpErrorConfig.handleError(err, this.userInfoService)));
   }
+
+    getSleepStages(sleepId: string) {
+        return this.http.get<SleepStage[]>(this.environment.fitnessService() + '/sleeps/' + sleepId + '/stages')
+            .pipe(catchError(err => this.httpErrorConfig.handleError(err, this.userInfoService)));
+    }
 }
