22216d198a0ca9c9f69129d272b19ffafca1ea86
Michael Baumberger <michael_baumberger@gmx.ch>
2025-06-15 17:10:36 +0200

    Remove unused imports `Observation` and `ObservationRegistry` from `UserService`.

--- usermanagement-service/src/main/kotlin/com/michibaum/usermanagement_service/UserService.kt
+++ usermanagement-service/src/main/kotlin/com/michibaum/usermanagement_service/UserService.kt
@@ -2,8 +2,6 @@ package com.michibaum.usermanagement_service
 
 import com.michibaum.permission_library.Permissions
 import com.michibaum.usermanagement_library.CreateUserDto
-import io.micrometer.observation.Observation
-import io.micrometer.observation.ObservationRegistry
 import io.micrometer.observation.annotation.Observed
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
