63e826c93c09eef7d0a5dca9645ea0c73b96ae8b
Michael Baumberger <michael_baumberger@gmx.ch>
2025-06-15 16:38:01 +0200

    Refactor event service to include event-participant and event-category mappings, add transactional annotation for consistency, adjust related repositories, and clean up service logic.

--- chess-service/src/main/kotlin/com/michibaum/chess_service/database/EventParticipantMappingRepository.kt
+++ chess-service/src/main/kotlin/com/michibaum/chess_service/database/EventParticipantMappingRepository.kt
@@ -2,10 +2,15 @@ package com.michibaum.chess_service.database
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import java.time.LocalDate
 import java.util.*
 
 interface EventParticipantMappingRepository: JpaRepository<EventParticipantsMapping, EventParticipantsMappingId>, JpaSpecificationExecutor<Event> {
-   
+    
+    @Modifying
+    @Query("DELETE FROM EventParticipantsMapping epm WHERE epm.event = :event")
+    fun deleteByEvent(event: Event)
+
 }
