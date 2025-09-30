a0b9c0e451867a2fb5c2880ceb16af1c7245505f
Michael Baumberger <michael_baumberger@gmx.ch>
2025-09-30 18:17:22 +0200

    Add new subdomain routes and update existing routing logic
    Added routing for new subdomains (`babymetal.ch`, `hanabie.ch`, `baumberger-software.*`, `einsiedeln.*`) with temporary redirects or placeholders. Reorganized and corrected route priority for `admin.michibaum.*` and other existing paths.

--- gateway-service/src/main/kotlin/com/michibaum/gatewayservice/config/RouterConfiguration.kt
+++ gateway-service/src/main/kotlin/com/michibaum/gatewayservice/config/RouterConfiguration.kt
@@ -33,52 +33,6 @@ class RouterConfiguration {
         circuitBreakerFactory: CircuitBreakerFactory<*, *>
     ): RouterFunction<ServerResponse> {
         return route()
-            // Add a redirect for "www.michibaum.*"
-            // First because all www should be redirected even robots.txt sitemap.xml
-            .route(host("www.michibaum.{tld}")) { req ->
-                val tld = req.pathVariable("tld")
-                val incomingScheme = req.uri().scheme
-                val path = req.uri().path
-                val query = req.uri().rawQuery?.let { "?$it" } ?: ""
-
-                val redirectUri = "$incomingScheme://michibaum.$tld$path$query"
-                ServerResponse.permanentRedirect(
-                    URI.create(redirectUri)
-                ).build()
-            }
-
-            // Special routes (Highest priority, must come first)
-            .GET("/robots.txt", robotsTxtController.robotsTxt())
-            .GET("/sitemap.xml", sitemapXmlController.sitemapXml())
-
-            // Specific services (Higher priority)
-            .route(host("admin.michibaum.*"), applyCircuitBreaker(lb(ADMIN.id).apply(http()),
-                ADMIN, circuitBreakerFactory))
-            .route(host("zipkin.michibaum.*")){request -> 
-                request.authenticateWithCircuitBreaker(servicesProperties.zipkinUrl, authFilter, circuitBreakerFactory,
-                    ZIPKIN, Permissions.ADMIN_SERVICE)
-            }
-            .route(host("grafana.michibaum.*")){request ->
-                request.authenticateWithCircuitBreaker(servicesProperties.grafanaUrl, authFilter, circuitBreakerFactory,
-                    GRAFANA, Permissions.ADMIN_SERVICE)
-            }
-            .route(host("prometheus.michibaum.*")){request ->
-                request.authenticateWithCircuitBreaker(servicesProperties.prometheusUrl, authFilter, circuitBreakerFactory,
-                    PROMETHEUS, Permissions.ADMIN_SERVICE)
-            }
-            .route(host("registry.michibaum.*"), applyCircuitBreaker(lb(REGISTRY.id).apply(http()),
-                REGISTRY, circuitBreakerFactory))
-            .route(host("authentication.michibaum.*"), applyCircuitBreaker(lb(AUTHENTICATION.id).apply(http()),
-                AUTHENTICATION, circuitBreakerFactory))
-            .route(host("usermanagement.michibaum.*"), applyCircuitBreaker(lb(USERMANAGEMENT.id).apply(http()),
-                USERMANAGEMENT, circuitBreakerFactory))
-            .route(host("chess.michibaum.*"), applyCircuitBreaker(lb(CHESS.id).apply(http()),
-                CHESS, circuitBreakerFactory))
-            .route(host("fitness.michibaum.*"), applyCircuitBreaker(lb(FITNESS.id).apply(http()),
-                FITNESS, circuitBreakerFactory))
-            .route(host("music.michibaum.*"), applyCircuitBreaker(lb(MUSIC.id).apply(http()),
-                MUSIC, circuitBreakerFactory))
-
             .route(host("babymetal.ch")) { _ -> // TODO implement custom fansite for babymetal.ch
 //                val path = req.uri().path
 //                val query = req.uri().rawQuery?.let { "?$it" } ?: ""
@@ -101,6 +55,52 @@ class RouterConfiguration {
             .route(host("einsiedeln.shop", "einsiedeln.store")) { _ -> // TODO implement custom service
                 ServerResponse.noContent().build()
             }
+            
+            // Add a redirect for "www.michibaum.*"
+            // First because all www should be redirected even robots.txt sitemap.xml
+            .route(host("www.michibaum.{tld}")) { req ->
+                val tld = req.pathVariable("tld")
+                val incomingScheme = req.uri().scheme
+                val path = req.uri().path
+                val query = req.uri().rawQuery?.let { "?$it" } ?: ""
+
+                val redirectUri = "$incomingScheme://michibaum.$tld$path$query"
+                ServerResponse.permanentRedirect(
+                    URI.create(redirectUri)
+                ).build()
+            }
+
+            // Special routes (Highest priority, must come first)
+            .GET("/robots.txt", robotsTxtController.robotsTxt())
+            .GET("/sitemap.xml", sitemapXmlController.sitemapXml())
+
+            // Specific services (Higher priority)
+            .route(host("zipkin.michibaum.*")){request -> 
+                request.authenticateWithCircuitBreaker(servicesProperties.zipkinUrl, authFilter, circuitBreakerFactory,
+                    ZIPKIN, Permissions.ADMIN_SERVICE)
+            }
+            .route(host("grafana.michibaum.*")){request ->
+                request.authenticateWithCircuitBreaker(servicesProperties.grafanaUrl, authFilter, circuitBreakerFactory,
+                    GRAFANA, Permissions.ADMIN_SERVICE)
+            }
+            .route(host("prometheus.michibaum.*")){request ->
+                request.authenticateWithCircuitBreaker(servicesProperties.prometheusUrl, authFilter, circuitBreakerFactory,
+                    PROMETHEUS, Permissions.ADMIN_SERVICE)
+            }
+            .route(host("admin.michibaum.*"), applyCircuitBreaker(lb(ADMIN.id).apply(http()),
+                ADMIN, circuitBreakerFactory))
+            .route(host("registry.michibaum.*"), applyCircuitBreaker(lb(REGISTRY.id).apply(http()),
+                REGISTRY, circuitBreakerFactory))
+            .route(host("authentication.michibaum.*"), applyCircuitBreaker(lb(AUTHENTICATION.id).apply(http()),
+                AUTHENTICATION, circuitBreakerFactory))
+            .route(host("usermanagement.michibaum.*"), applyCircuitBreaker(lb(USERMANAGEMENT.id).apply(http()),
+                USERMANAGEMENT, circuitBreakerFactory))
+            .route(host("chess.michibaum.*"), applyCircuitBreaker(lb(CHESS.id).apply(http()),
+                CHESS, circuitBreakerFactory))
+            .route(host("fitness.michibaum.*"), applyCircuitBreaker(lb(FITNESS.id).apply(http()),
+                FITNESS, circuitBreakerFactory))
+            .route(host("music.michibaum.*"), applyCircuitBreaker(lb(MUSIC.id).apply(http()),
+                MUSIC, circuitBreakerFactory))
 
             // Catch-all for main website (Lowest priority, must come last)
             .route(host("michibaum.*"), applyCircuitBreaker(lb(WEBSITE.id).apply(http()),
