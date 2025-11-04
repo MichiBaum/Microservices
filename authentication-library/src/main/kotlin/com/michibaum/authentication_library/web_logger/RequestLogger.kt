34b5f81d8f91ce92c1cff354dc3ab6f887a1e243
Michael Baumberger <michael_baumberger@gmx.ch>
2025-11-04 10:54:18 +0100

    Add web logger components: `Request`, `Response` interfaces, loggers, and mappers for request/response transformation

---
+++ authentication-library/src/main/kotlin/com/michibaum/authentication_library/web_logger/RequestLogger.kt
@@ -0,0 +1,9 @@
+package com.michibaum.authentication_library.web_logger
+
+class RequestLogger {
+    
+    fun <R: Request> log(request: R){
+        
+    }
+    
+}
---
+++ authentication-library/src/main/kotlin/com/michibaum/authentication_library/web_logger/Response.java
@@ -0,0 +1,4 @@
+package com.michibaum.authentication_library.web_logger;
+
+public interface Response {
+}
