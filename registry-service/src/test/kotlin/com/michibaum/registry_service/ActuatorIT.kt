b51fc1bc2bd34c3a946f42065db8a6eea03a0dc8
Michael Baumberger <michael_baumberger@gmx.ch>
2026-03-14 13:03:51 +0100

    Replace MockMvc tests with TestRestTemplate for actuator endpoint tests across services and update configurations accordingly

--- registry-service/src/test/kotlin/com/michibaum/registry_service/ActuatorIT.kt
+++ registry-service/src/test/kotlin/com/michibaum/registry_service/ActuatorIT.kt
@@ -1,26 +1,44 @@
 package com.michibaum.registry_service
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.michibaum.authentication_library.JwsValidationSuccess
+import com.michibaum.authentication_library.security.jwt.JwsValidator
+import com.michibaum.permission_library.Permissions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.resttestclient.TestRestTemplate
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
-import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import java.util.*
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 
-@AutoConfigureMockMvc
-@SpringBootTest(properties = [
-    "spring.boot.admin.service.username=someUsername",
-    "spring.boot.admin.service.password=somePasswööörd"
-])
+/**
+ * End-to-End Tests with TestRestTemplate are used due to MockMvc not including Actuator endpoints.
+ * See https://docs.spring.io/spring-framework/reference/testing/mockmvc/vs-end-to-end-integration-tests.html
+ */
+@AutoConfigureTestRestTemplate
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    properties = [
+        "spring.boot.admin.service.username=admin_username",
+        "spring.boot.admin.service.password=admin_password"
+    ]
+)
 class ActuatorIT {
 
     @Autowired
-    lateinit var mockMvc: MockMvc
+    lateinit var testRestTemplate: TestRestTemplate
+
+    @MockitoBean
+    lateinit var jwsValidator: JwsValidator
 
     @ParameterizedTest
     @ValueSource(strings = ["/actuator", "/actuator/health", "/actuator/info"])
@@ -28,45 +46,52 @@ class ActuatorIT {
         // GIVEN
 
         // WHEN
-        mockMvc.perform(get(endpoint))
-            .andExpect(status().isUnauthorized)
+        val response = testRestTemplate.getForEntity(endpoint, String::class.java)
 
         // THEN
-
+        assertEquals(HttpStatus.UNAUTHORIZED, response.statusCode)
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["/actuator", "/actuator/health", "/actuator/info"])
     fun `actuator endpoints with basic authentication return 200`(endpoint: String){
         // GIVEN
-        val basicAuth = "someUsername:somePasswööörd"
-        val basicAuthEncoded = Base64.getEncoder().encodeToString(basicAuth.toByteArray())
+        val response = testRestTemplate
+            .withBasicAuth("admin_username", "admin_password")
+            .getForEntity(endpoint, String::class.java)
 
         // WHEN
-        mockMvc.perform(
-            get(endpoint)
-                .header("Authorization", "Basic $basicAuthEncoded")
-        )
-            .andExpect(status().isOk)
 
         // THEN
-
+        assertEquals(HttpStatus.OK, response.statusCode)
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["/actuator", "/actuator/health", "/actuator/info"])
     fun `actuator endpoints with not enough authorities return 403`(endpoint: String){
         // GIVEN
+        val token = JWT.create()
+            .withSubject("admin_username")
+            .withClaim("userId", "isdnfvin")
+            .withClaim("permissions", listOf(Permissions.CHESS_SERVICE.name))
+            .sign(Algorithm.none())
+
+        `when`(jwsValidator.validate(anyString())).thenReturn(JwsValidationSuccess())
+
+        val headers = HttpHeaders().apply {
+            setBearerAuth(token)
+        }
 
         // WHEN
-        mockMvc.perform(
-            get(endpoint)
-                .with(user("user").authorities(SimpleGrantedAuthority("SOME_OTHER_PERMISSION")))
+        val response = testRestTemplate.exchange(
+            endpoint,
+            HttpMethod.GET,
+            HttpEntity<Void>(headers),
+            String::class.java
         )
-            .andExpect(status().isForbidden)
 
         // THEN
-
+        assertEquals(HttpStatus.FORBIDDEN, response.statusCode)
     }
 
 }
