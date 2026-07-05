0a332b78279d41de3a1108c76c28a8d720deda7b
Michael Baumberger <michael_baumberger@gmx.ch>
2026-07-05 10:58:19 +0200

    Make `JwsValidator` methods open and improve error logging during public key reload

--- authentication-library/src/main/kotlin/com/michibaum/authentication_library/security/jwt/JwsValidator.kt
+++ authentication-library/src/main/kotlin/com/michibaum/authentication_library/security/jwt/JwsValidator.kt
@@ -18,31 +18,37 @@ open class JwsValidator(
     private val authenticationClient: AuthenticationClient
 ): JwsValidator() {
 
-    val logger: Logger = org.slf4j.LoggerFactory.getLogger(this.javaClass)
+    companion object {
+        val logger: Logger = org.slf4j.LoggerFactory.getLogger(JwsValidator::class.java)
+    }
 
     private var publicKey: RSAPublicKey? = null
 
-    fun reloadPublicKey() {
+    open fun reloadPublicKey() {
         val dto = try {
-             authenticationClient.publicKey()
-        } catch (ex: FeignException.Unauthorized){
+            logger.info("JwsValidator reloading public key")
+            authenticationClient.publicKey()
+        } catch (ex: FeignException.Unauthorized) {
             logger.error("JwsValidator could not reload public key: ${ex.message}", ex)
             return
         } catch (ex: FeignException.ServiceUnavailable) {
             logger.error("JwsValidator could not reach authentication server: ${ex.message}")
+            return
+        } catch (ex: Exception) {
+            logger.error("JwsValidator unexpected error during public key reload: ${ex.message}", ex)
             return
         }
         val pubkey: PublicKey = KeyFactory.getInstance(dto.algorithm).generatePublic(X509EncodedKeySpec(dto.key))
         publicKey = pubkey as RSAPublicKey
     }
 
-    fun validate(token: String): JwsValidationResult {
+    open fun validate(token: String): JwsValidationResult {
         return validate(token, publicKey)
     }
 
     @Observed(name = "jws.validator.update.public.key")
     @Scheduled(fixedRate = 2, timeUnit = TimeUnit.MINUTES)
-    fun updatePublicKey(){
+    open fun updatePublicKey(){
         reloadPublicKey()
     }
 
