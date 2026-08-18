33e36016b05740462b8a3372f0bbf9c6ba7c8a26
Michael Baumberger <michael_baumberger@gmx.ch>
2026-08-18 20:05:38 +0200

    Freeze ArchUnit rules and refine rule definitions for stricter architecture enforcement

--- archunittests/src/main/kotlin/com/michibaum/archunittests/NamingRules.kt
+++ archunittests/src/main/kotlin/com/michibaum/archunittests/NamingRules.kt
@@ -3,29 +3,30 @@ package com.michibaum.archunittests
 import com.tngtech.archunit.junit.ArchTest
 import com.tngtech.archunit.lang.ArchRule
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
+import com.tngtech.archunit.library.freeze.FreezingArchRule.freeze
 
 class NamingRules {
     
     companion object {
 
         @ArchTest
-        val controllerNaming: ArchRule = classes().that().areAnnotatedWith("org.springframework.web.bind.annotation.RestController")
+        val controllerNaming: ArchRule = freeze(classes().that().areAnnotatedWith("org.springframework.web.bind.annotation.RestController")
             .or().areAnnotatedWith("org.springframework.stereotype.Controller")
             .should().haveSimpleNameEndingWith("Controller")
-            .allowEmptyShould(true)
+            .allowEmptyShould(true))
 
         @ArchTest
-        val repositoryNaming: ArchRule = 
-            classes().that().areAssignableTo("org.springframework.data.repository.Repository")
+        val repositoryNaming: ArchRule =
+            freeze(classes().that().areAssignableTo("org.springframework.data.repository.Repository")
             .or().areAnnotatedWith("org.springframework.stereotype.Repository")
             .should().haveSimpleNameEndingWith("Repository")
-            .allowEmptyShould(true)
+            .allowEmptyShould(true))
 
         @ArchTest
-        val repositoryNaming2: ArchRule = classes().that().haveSimpleNameEndingWith("Repository")
+        val repositoryNaming2: ArchRule = freeze(classes().that().haveSimpleNameEndingWith("Repository")
             .should().beAssignableTo("org.springframework.data.repository.Repository")
             .andShould().beAnnotatedWith("org.springframework.stereotype.Repository")
-            .allowEmptyShould(true)
+            .allowEmptyShould(true))
     }
     
 }
