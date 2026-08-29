a10a88cff2bc788827be0805544fa21ea3d0ed93
Michael Baumberger <michael_baumberger@gmx.ch>
2026-08-29 13:06:42 +0200

    Enable scheduling for VPN service, refine JWS validation, update gateway connection limits, and adjust Kubernetes configuration properties.

--- vpn-service/src/main/kotlin/com/michibaum/vpn_service/VpnServiceApplication.kt
+++ vpn-service/src/main/kotlin/com/michibaum/vpn_service/VpnServiceApplication.kt
@@ -1,15 +1,16 @@
 package com.michibaum.vpn_service
 
-import com.michibaum.vpn_service.app.kubernetes.KubernetesProperties
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableConfigurationProperties(value = [KubernetesProperties::class])
+@EnableScheduling
+@EnableConfigurationProperties
 class VpnServiceApplication
 
 fun main(args: Array<String>) {
